f4cbb080aebdb6399a5b11ff95eec1a4226ceeed
luisestebanveragomez <minos_93105@hotmail.com>
2022-12-30 17:48:03 -0500

    text build colors

--- packages/vel-card/web-dev-server.config.mjs
+++ packages/vel-card/web-dev-server.config.mjs
@@ -1,6 +1,8 @@
 // import { hmrPlugin, presets } from '@open-wc/dev-server-hmr';
 /** Use Hot Module replacement by adding --hmr to the start command */
+import { nodeResolve } from '@rollup/plugin-node-resolve'
 const hmr = process.argv.includes('--hmr');
+import url from 'rollup-plugin-url'
 
 export default /** @type {import('@web/dev-server').DevServerConfig} */ ({
   open: './packages/vel-card/demo/',
@@ -11,12 +13,15 @@ export default /** @type {import('@web/dev-server').DevServerConfig} */ ({
   rootDir: '../../',
 
   /** Compile JS for older browsers. Requires @web/dev-server-esbuild plugin */
-  // esbuildTarget: 'auto'
+  esbuildTarget: 'auto',
 
   /** Set appIndex to enable SPA routing */
   // appIndex: 'demo/index.html',
 
   plugins: [
+    url({
+      include: ['**/*.ttf'],
+    }),
     /** Use Hot Module Replacement by uncommenting. Requires @open-wc/dev-server-hmr plugin */
     // hmr && hmrPlugin({ exclude: ['**/*/node_modules/**/*'], presets: [presets.litElement] }),
   ],
