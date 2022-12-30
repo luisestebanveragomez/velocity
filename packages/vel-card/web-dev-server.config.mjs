ded5c1d6e31166a41aafad9ad064e8fd857703a0
luisestebanveragomez <minos_93105@hotmail.com>
2022-12-30 15:58:16 -0500

    fix route fonts

--- packages/vel-card/web-dev-server.config.mjs
+++ packages/vel-card/web-dev-server.config.mjs
@@ -3,13 +3,12 @@
 const hmr = process.argv.includes('--hmr');
 
 export default /** @type {import('@web/dev-server').DevServerConfig} */ ({
-  open: '/demo/',
+  open: './packages/vel-card/demo/',
   /** Use regular watch mode if HMR is not enabled. */
   watch: !hmr,
   /** Resolve bare module imports */
-  nodeResolve: {
-    exportConditions: ['browser', 'development'],
-  },
+  nodeResolve: true,
+  rootDir: '../../',
 
   /** Compile JS for older browsers. Requires @web/dev-server-esbuild plugin */
   // esbuildTarget: 'auto'
